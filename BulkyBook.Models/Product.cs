819048e3c4bb0686a9a437509d132182ae66dd69
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-04-16 18:22:36 +0200

    Product : addded Display attribute (no localization yet).

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -27,24 +27,29 @@ namespace BulkyBook.Models
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "List Price")]
         public double ListPrice { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 1-49")]
         public double Price { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 50-99")]
         public double Price50 { get; set; }
 
         [Required]
         [Range(1, 10000)]
+        [Display(Name = "Price for 100+")]
         public double Price100 { get; set; }
 
         [ValidateNever]
         public string ImageUrl { get; set; }
 
         [Required]
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         [ForeignKey("CategoryId")]
@@ -52,6 +57,7 @@ namespace BulkyBook.Models
         public Category Category { get; set; }
 
         [Required]
+        [Display(Name = "Cover Type")]
         public int CoverTypeId { get; set; }
 
         [ForeignKey("CoverTypeId")]
