ab0cd48d0406fd96f0b570a5c168cee1220c2f77
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-04-24 19:59:49 +0200

    Added CompanyController and corresponding views.

---
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,101 @@
+﻿using BulkyBook.DataAccess.Repository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        const int CreateCompanyId = 0;
+
+        readonly IUnitOfWork _db;
+
+        public CompanyController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Company company;
+            if (id == CreateCompanyId)
+            {
+                company = new Company();
+            }
+            else {
+                company = _db.Company.Find(id);
+                if (company == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Company company)
+        {
+            if (!ModelState.IsValid)
+                return View(company);
+
+            if (company.Id == CreateCompanyId)
+            {
+                // create new company
+                _db.Company.Add(company);
+                _db.Save();
+
+                TempData["success"] = $"Company '{company.Name}' updated successfully.";
+            }
+            else
+            {
+                // update existing company
+                _db.Company.Update(company);
+                _db.Save();
+
+                TempData["success"] = $"Company '{company.Name}' updated successfully.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Company.GetAll().ToList() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var target = _db.Company.Find(id);
+            if (target == null)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Specified Company not found."
+                });
+            }
+
+            _db.Company.Remove(target);
+            _db.Save();
+
+            return Json(new
+            {
+                success = true,
+                message = "Company deleted successfully."
+            });
+        }
+
+    }
+}
---
+++ BulkyBookWeb/wwwroot/js/company.js
@@ -0,0 +1,91 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#companyTable').DataTable({
+        "ajax": {
+            "url": "/Admin/Company/GetAll",
+        },
+        "columns": [
+            {
+                "data": "name",
+                "width": "15%"
+            },
+            {
+                "data": "streetAddress",
+                "width": "15%"
+            },
+            {
+                "data": "city",
+                "width": "15%"
+            },
+            {
+                "data": "state",
+                "width": "10%"
+            },
+            {
+                "data": "postalCode",
+                "width": "10%"
+            },
+            {
+                "data": "phoneNumber",
+                "width": "10%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="w-75 btn-group" role="group">
+                        <a href="/Admin/Company/Edit/${data}" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i>
+                            Edit
+                        </a>
+                        <a onClick=Delete('/Admin/Company/Delete/${data}') class="btn btn-danger mx-2">
+                            <i class="bi bi-trash"></i>
+                            Delete
+                        </a>
+                    </div>
+                    `;
+                },
+                "width" : "15%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (response) {
+                    if (response.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(response.message);
+                        //Swal.fire(
+                        //    'Deleted!',
+                        //    'Your product has been deleted.',
+                        //    'success'
+                        //)
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+
+        }
+    })
+}
