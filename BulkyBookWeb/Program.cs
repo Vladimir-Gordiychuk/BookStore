cf8584e07ed56389fdec2aae2c9b7d1b9180d431
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-04-24 19:59:32 +0200

    Minor code cleanup in Program.cs.

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -5,16 +5,18 @@ using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 builder.Services
     .AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
