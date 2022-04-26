ed016198844c82abff97612c25bb834ad6b82bd1
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-04-26 16:16:31 +0200

    Switched to custom Identity config. Added dummy EmailSender.

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility
 {
+    /// <summary>
+    /// Static Details.
+    /// </summary>
     public static class SD
     {
+        public const string RoleCustomerIdividual = "Individual";
+        public const string RoleCustomerCompany = "Company";
+        public const string RoleAdmin = "Admin";
+        public const string RoleEmployee = "Employee";
     }
 }
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -2,15 +2,20 @@ using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BulkyBook.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IEmailSender, DummyEmailSender>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
 builder.Services
-    .AddDefaultIdentity<IdentityUser>()
+    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
@@ -18,6 +23,8 @@ builder.Services.AddControllersWithViews();
 
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
