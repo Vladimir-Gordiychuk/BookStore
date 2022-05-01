b2e1719961313d03dee953a796e08d31244062d5
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-05-01 14:31:44 +0200

    Added CartController and a template for Index view.

---
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        readonly IUnitOfWork _db;
+
+        public CartController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
