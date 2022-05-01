bc1d3286579b85b6392210f4e1372eb03aacb43b
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-05-01 15:12:00 +0200

    CartController : updated Index method and its view.

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,10 +1,15 @@
 ﻿using BulkyBook.DataAccess.Repository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
         readonly IUnitOfWork _db;
@@ -13,11 +18,32 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
         {
             _db = db;
         }
-
-        [Authorize]
+        
         public IActionResult Index()
         {
-            return View();
+            var userId = GetCurrentUserId();
+
+            var cart = new ShoppingCartVm
+            {
+                Items = _db.ShoppingCart
+                    .Where(
+                        item => item.ApplicationUserId == userId,
+                        nameof(ShoppingCart.Product))
+                    .ToList()
+            };
+
+            return View(cart);
         }
+
+        private string GetCurrentUserId()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            Debug.Assert(claimsIdentity != null, "User is required to be logged in.");
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            Debug.Assert(claim != null, "All valid users are supposed to have an Id.");
+            return claim.Value;
+        }
+
+
     }
 }
