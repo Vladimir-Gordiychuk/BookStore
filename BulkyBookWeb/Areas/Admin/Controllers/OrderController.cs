78649b1a06d86a678bc2ddda2669a927caf103d0
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-05-08 15:13:57 +0200

    OrderController : implemented proper order filtering for different user roles.

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,11 +2,16 @@
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using BulkyBook.Utility;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class OrderController : Controller
     {
         readonly IUnitOfWork _db;
@@ -24,29 +29,62 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            Expression<Func<OrderHeader, bool>> filter = (order) => true;
+            Expression<Func<OrderHeader, bool>> filterExpression = GetStatusFilterExpression(status);
+
+            IEnumerable<OrderHeader> orders;
+
+            if (User.IsInRole(SD.RoleAdmin) || User.IsInRole(SD.RoleEmployee))
+            {
+                orders = _db.OrderHeader
+                    .Where(filterExpression, includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var userId = GetCurrentUserId();
+
+                var filterFunction = filterExpression.Compile();
+
+                orders = _db.OrderHeader
+                    .Where(order => order.ApplicationUserId == userId, includeProperties: "ApplicationUser")
+                    .Where(filterFunction);
+            }
+
+            return Json(new
+            {
+                data = orders
+            });
+        }
+
+        private static Expression<Func<OrderHeader, bool>> GetStatusFilterExpression(string status)
+        {
+            Expression<Func<OrderHeader, bool>> filterExpression = (order) => true;
 
             switch (status)
             {
                 case "inprocess":
-                    filter = (order) => order.OrderStatus == SD.StatusInProcess;
+                    filterExpression = (order) => order.OrderStatus == SD.StatusInProcess;
                     break;
                 case "pending":
-                    filter = (order) => order.PaymentStatus == SD.PaymentStatusDelayedPayment;
+                    filterExpression = (order) => order.PaymentStatus == SD.PaymentStatusDelayedPayment;
                     break;
                 case "completed":
-                    filter = (order) => order.OrderStatus == SD.StatusShipped;
+                    filterExpression = (order) => order.OrderStatus == SD.StatusShipped;
                     break;
                 case "approved":
-                    filter = (order) => order.OrderStatus == SD.StatusApproved;
+                    filterExpression = (order) => order.OrderStatus == SD.StatusApproved;
                     break;
             }
 
-            return Json(new {
-                data = _db.OrderHeader
-                    .Where(filter, includeProperties: "ApplicationUser")
-                    .ToList()
-            });
+            return filterExpression;
+        }
+
+        private string GetCurrentUserId()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            Debug.Assert(claimsIdentity != null, "User is required to be logged in.");
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            Debug.Assert(claim != null, "All valid users are supposed to have an Id.");
+            return claim.Value;
         }
     }
 }
