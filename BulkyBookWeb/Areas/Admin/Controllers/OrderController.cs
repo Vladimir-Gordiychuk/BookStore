8994016b2a030a8327d06e71a53817551b22c595
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-05-08 14:02:04 +0200

    Added "Orders" page in Admin area to see list of orders and their status.

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        readonly IUnitOfWork _db;
+
+        public OrderController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList() });
+        }
+    }
+}
---
+++ BulkyBookWeb/wwwroot/js/order.js
@@ -0,0 +1,69 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function yyyymmdd(date) {
+    var mm = date.getMonth() + 1; // getMonth() is zero-based
+    var dd = date.getDate();
+
+    return [date.getFullYear(),
+        (mm > 9 ? '' : '0') + mm,
+        (dd > 9 ? '' : '0') + dd
+        ].join('.');
+}
+
+function loadDataTable() {
+    dataTable = $('#orderTable').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll",
+        },
+        "columns": [
+            {
+                "data": "id",
+                "width": "5%"
+            },
+            {
+                "data": "name",
+                "width": "15%"
+            },
+            {
+                "data": "phoneNumber",
+                "width": "10%"
+            },
+            {
+                "data": "applicationUser.email",
+                "width": "15%"
+            },
+            {
+                "data": "orderStatus",
+                "width": "10%"
+            },
+            {
+                "data": "orderDate",
+                "width": "7%",
+                "render": (date) => {
+                    return yyyymmdd(new Date(date));
+                }
+            },
+            {
+                "data": "orderTotal",
+                "width": "7%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="w-75 btn-group" role="group">
+                        <a href="/Admin/Order/Details/${data}" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i>
+                        </a>
+                    </div>
+                    `;
+                },
+                "width" : "5%"
+            }
+        ]
+    });
+}
